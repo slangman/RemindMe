d30c2fb058b22253b0aa4cf0774ff1dcb3457b87
slangman <mc.slangman@gmail.com>
2018-03-09 20:20:37 +0600

    Trying to solve first task.

---
+++ app/src/main/java/com/hustleind/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hustleind.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by AttenCHUN on 09.03.2018.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
