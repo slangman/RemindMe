1b7a1df1cb1001d4f2758d0ca1079f6f7efe3929
slangman <mc.slangman@gmail.com>
2018-03-12 20:13:22 +0600

    Created toolbar.

--- app/src/main/java/com/hustleind/remindme/MainActivity.java
+++ app/src/main/java/com/hustleind/remindme/MainActivity.java
@@ -2,16 +2,34 @@ package com.hustleind.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by AttenCHUN on 09.03.2018.
  */
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
